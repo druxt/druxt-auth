fad2dc5488d7961720995ac650e4787ca3dcec40
Stuart Clark <stu@rtclark.net>
2022-08-12 04:40:52 +0000

    chore(#20): update example

--- example/nuxt/pages/index.vue
+++ example/nuxt/pages/index.vue
@@ -3,6 +3,8 @@
     <template v-if="!this.$nuxt.$auth.loggedIn">
       <h1>You are not logged in :(</h1>
       <button @click="$auth.loginWith('drupal-authorization_code')">Click here to log in</button>
+      <pre><code>Username: admin
+Password: password</code></pre>
     </template>
 
     <template v-else>
